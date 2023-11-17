634cb94d7682bf9577eb102322ecfb49e01dc7b1
koray kilic <korayerdem89@gmail.com>
2023-11-17 17:08:46 +0300

    end of the day

--- src/screens/onboardingScreens/OnboardingScreens.js
+++ src/screens/onboardingScreens/OnboardingScreens.js
@@ -1,15 +1,37 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, ImageBackground, View, Dimensions } from "react-native";
+import { getStatusBarHeight } from "react-native-status-bar-height";
 import React from "react";
-import OnboardingLayout from "../../layouts/onboarding/OnboardingLayout";
+import ButtonComponent from "../../components/ButtonComponent";
 import TextComponent from "../../components/TextComponent";
+const { width, height } = Dimensions.get("window");
+
 const OnboardingScreens = () => {
   return (
-    <OnboardingLayout>
-      <TextComponent size="2xl">DENEME YAZISI</TextComponent>
-    </OnboardingLayout>
+    <ImageBackground
+      source={require("../../../assets/OnboardingBackgrounds/getStarted_bg.png")}
+      style={styles.image}
+      resizeMode="stretch"
+    >
+      <View style={styles.container}>
+        <TextComponent>asdsa</TextComponent>
+        <ButtonComponent title="Get Started" />
+      </View>
+    </ImageBackground>
   );
 };
 
 export default OnboardingScreens;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingHorizontal: 0.064 * width,
+    paddingTop: getStatusBarHeight() + 12,
+    paddingBottom: 42,
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+});
