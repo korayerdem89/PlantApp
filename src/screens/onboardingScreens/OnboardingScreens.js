41a4cfae07d9f7b1337a67f2fbab59cdb365d03c
koray kilic <korayerdem89@gmail.com>
2023-11-19 21:41:12 +0300

    paywall UI is ok

--- src/screens/onboardingScreens/OnboardingScreens.js
+++ src/screens/onboardingScreens/OnboardingScreens.js
@@ -7,11 +7,13 @@ import {
   StyleSheet,
   ScrollView,
   Image,
+  TouchableOpacity,
 } from "react-native";
 import { getStatusBarHeight } from "react-native-status-bar-height";
 import ButtonComponent from "../../components/ButtonComponent";
 import TextComponent from "../../components/TextComponent";
 import { colors } from "../../constants/theme";
+import { LinearGradient } from "expo-linear-gradient";
 
 const { width, height } = Dimensions.get("window");
 
@@ -62,7 +64,7 @@ const onboardingItems = [
 const OnboardingScreens = () => {
   const flatListRef = useRef();
   const [currentIndex, setCurrentIndex] = useState(0);
-
+  const [subscriptionType, setSubscriptionType] = useState("year");
   const handlePress = useCallback(() => {
     setCurrentIndex((prevIndex) => {
       const nextIndex = prevIndex + 1;
@@ -76,6 +78,11 @@ const OnboardingScreens = () => {
       return prevIndex;
     });
   }, []);
+
+  const handleSelect = (value) => {
+    setSubscriptionType(value);
+    return null;
+  };
 
   return (
     <View style={styles.container}>
@@ -96,52 +103,242 @@ const OnboardingScreens = () => {
             <View style={styles.renderContainer}>
               <TextComponent>{item.title}</TextComponent>
               {item.paywall && (
-                <View style={styles.paywallContainer}>
-                  <TextComponent
-                    size="l"
-                    textStyle="light"
-                    style={{ color: "white", lineHeight: 35 }}
-                  >
-                    <TextComponent textStyle="bold" size="l">
-                      PlantApp{" "}
+                <>
+                  <View style={styles.closeScreen}>
+                    <TouchableOpacity>
+                      <Image
+                        source={require("../../../assets/closeIcon.png")}
+                        style={{ width: 24, height: 24 }}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.paywallContainer}>
+                    <TextComponent
+                      size="l"
+                      textStyle="light"
+                      style={{ color: "white", lineHeight: 35 }}
+                    >
+                      <TextComponent textStyle="bold" size="l">
+                        PlantApp{" "}
+                      </TextComponent>
+                      Premium
                     </TextComponent>
-                    Premium
-                  </TextComponent>
-                  <TextComponent
-                    style={{ color: "rgba(255,255,255,0.7)" }}
-                    size="s"
-                    textStyle="light"
-                  >
-                    Access All Features
-                  </TextComponent>
-                  <ScrollView horizontal>
-                    {item.paywallSlide.map((slide) => {
-                      return (
-                        <View style={styles.featuresCard} key={slide.id}>
-                          <Image
-                            source={slide.icon}
-                            style={styles.paywallIcon}
-                            resizeMode="cover"
-                          />
-                          <TextComponent
-                            size="m"
-                            textStyle="medium"
-                            style={{ color: "white" }}
+                    <TextComponent
+                      style={{ color: "rgba(255,255,255,0.7)" }}
+                      size="s"
+                      textStyle="light"
+                    >
+                      Access All Features
+                    </TextComponent>
+                    <ScrollView
+                      showsHorizontalScrollIndicator={false}
+                      horizontal
+                    >
+                      {item.paywallSlide.map((slide) => {
+                        return (
+                          <View style={styles.featuresCard} key={slide.id}>
+                            <Image
+                              source={slide.icon}
+                              style={styles.paywallIcon}
+                              resizeMode="cover"
+                            />
+                            <TextComponent
+                              size="m"
+                              textStyle="medium"
+                              style={{ color: "white" }}
+                            >
+                              {slide.title}
+                            </TextComponent>
+                            <TextComponent
+                              size="xs"
+                              textStyle="light"
+                              style={{
+                                color: "rgba(255,255,255,0.7)",
+                                paddingTop: 2,
+                              }}
+                            >
+                              {slide.subtitle}
+                            </TextComponent>
+                          </View>
+                        );
+                      })}
+                    </ScrollView>
+                    <View style={styles.subscriptionCardContainer}>
+                      <TouchableOpacity
+                        onPress={handleSelect.bind(this, "month")}
+                      >
+                        <LinearGradient
+                          style={{
+                            borderRadius: height > 720 ? 14 : 12,
+                            marginBottom: 16,
+                          }}
+                          colors={
+                            subscriptionType === "year"
+                              ? [
+                                  "rgba(255,255,255,0.11)",
+                                  "rgba(255,255,255,0.14)",
+                                  "rgba(255,255,255,0.07)",
+                                ]
+                              : [
+                                  "transparent",
+                                  "rgba(40,175,110,0.15)",
+                                  "rgba(40,175,110,0.7)",
+                                ]
+                          }
+                          start={
+                            subscriptionType === "year"
+                              ? { x: 0, y: 0 }
+                              : { x: 0, y: 1 }
+                          }
+                          end={{ x: 1, y: 0 }}
+                          locations={
+                            subscriptionType === "year"
+                              ? null
+                              : [0.3, 0.35, 0.85]
+                          }
+                        >
+                          <View
+                            style={[
+                              styles.subscriptionCard,
+                              {
+                                borderColor:
+                                  subscriptionType === "month"
+                                    ? "#28AF6E"
+                                    : "rgba(255,255,255,0.3)",
+                              },
+                            ]}
                           >
-                            {slide.title}
-                          </TextComponent>
-                          <TextComponent
-                            size="xs"
-                            textStyle="light"
-                            style={{ color: "rgba(255,255,255,0.7)" }}
+                            <View
+                              style={
+                                subscriptionType === "month"
+                                  ? styles.activeRadioButton
+                                  : styles.radioButton
+                              }
+                            >
+                              {subscriptionType === "month" && (
+                                <View style={styles.activeRadioDot}></View>
+                              )}
+                            </View>
+                            <View style={styles.descriptionContainer}>
+                              <TextComponent
+                                size="s"
+                                textStyle="medium"
+                                style={{ color: "#FFFFFF" }}
+                              >
+                                1 Month
+                              </TextComponent>
+                              <TextComponent
+                                size="xs"
+                                textStyle="light"
+                                style={{ color: "rgba(255,255,255,0.7)" }}
+                              >
+                                $2.99/month,{" "}
+                                <TextComponent size="xs">
+                                  auto renewable
+                                </TextComponent>
+                              </TextComponent>
+                            </View>
+                          </View>
+                        </LinearGradient>
+                      </TouchableOpacity>
+
+                      <TouchableOpacity
+                        onPress={handleSelect.bind(this, "year")}
+                      >
+                        <LinearGradient
+                          style={{ borderRadius: height > 720 ? 14 : 12 }}
+                          colors={
+                            subscriptionType === "month"
+                              ? [
+                                  "rgba(255,255,255,0.11)",
+                                  "rgba(255,255,255,0.14)",
+                                  "rgba(255,255,255,0.07)",
+                                ]
+                              : [
+                                  "transparent",
+                                  "rgba(40,175,110,0.15)",
+                                  "rgba(40,175,110,0.7)",
+                                ]
+                          }
+                          start={
+                            subscriptionType === "month"
+                              ? { x: 0, y: 0 }
+                              : { x: 0, y: 1 }
+                          }
+                          end={{ x: 1, y: 0 }}
+                          locations={
+                            subscriptionType === "month"
+                              ? null
+                              : [0.3, 0.35, 0.85]
+                          }
+                        >
+                          <View
+                            style={[
+                              styles.subscriptionCard2,
+                              {
+                                borderColor:
+                                  subscriptionType === "year"
+                                    ? "#28AF6E"
+                                    : "rgba(255,255,255,0.3)",
+                              },
+                            ]}
                           >
-                            {slide.subtitle}
-                          </TextComponent>
-                        </View>
-                      );
-                    })}
-                  </ScrollView>
-                </View>
+                            <View
+                              style={
+                                subscriptionType === "year"
+                                  ? styles.activeRadioButton
+                                  : styles.radioButton
+                              }
+                            >
+                              {subscriptionType === "year" && (
+                                <View style={styles.activeRadioDot}></View>
+                              )}
+                            </View>
+                            <View style={styles.descriptionContainer}>
+                              <TextComponent
+                                size="s"
+                                textStyle="medium"
+                                style={{ color: "#FFFFFF" }}
+                              >
+                                1 Year
+                              </TextComponent>
+                              <TextComponent
+                                size="xs"
+                                textStyle="light"
+                                style={{ color: "rgba(255,255,255,0.7)" }}
+                              >
+                                $2.99/month,{" "}
+                                <TextComponent size="xs">
+                                  auto renewable
+                                </TextComponent>
+                              </TextComponent>
+                            </View>
+                            <View
+                              style={{
+                                backgroundColor: "#28AF6E",
+                                width: "26%",
+                                position: "absolute",
+                                right: 0,
+                                top: 0,
+                                borderBottomLeftRadius: height > 720 ? 20 : 18,
+                                justifyContent: "center",
+                                alignItems: "center",
+                              }}
+                            >
+                              <TextComponent
+                                textStyle="medium"
+                                style={{ color: "#FFFFFF", padding: 4 }}
+                                size="xs"
+                              >
+                                Save 50%
+                              </TextComponent>
+                            </View>
+                          </View>
+                        </LinearGradient>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </>
               )}
             </View>
           </ImageBackground>
@@ -204,7 +401,7 @@ const styles = StyleSheet.create({
     width,
   },
   paywallContainer: {
-    bottom: 146,
+    bottom: height > 720 ? 155 : 150,
     height: height > 720 ? 380 : 320,
   },
   bottomContainer: {
@@ -254,7 +451,7 @@ const styles = StyleSheet.create({
     marginRight: 8,
     backgroundColor: "rgba(255,255,255,0.08)",
     width: 0.39 * width,
-    height: 0.31 * width,
+    height: 0.16 * height,
     borderRadius: 14,
     justifyContent: "space-around",
   },
@@ -262,5 +459,55 @@ const styles = StyleSheet.create({
     width: 0.08 * width,
     height: 0.08 * width,
     marginBottom: 16,
+  },
+  subscriptionCard: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 0.5,
+    borderRadius: height > 720 ? 14 : 12,
+    backgroundColor: "rgba(255,255,255,0.05)",
+    paddingVertical: height > 720 ? 11 : 10,
+    paddingHorizontal: height > 720 ? 16 : 13,
+  },
+  subscriptionCard2: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1.5,
+    borderRadius: height > 720 ? 14 : 12,
+    backgroundColor: "rgba(255,255,255,0.05)",
+    paddingVertical: height > 720 ? 11 : 10,
+    paddingLeft: height > 720 ? 16 : 13,
+    overflow: "hidden",
+  },
+  subscriptionCardContainer: {
+    top: 5,
+  },
+  radioButton: {
+    backgroundColor: "rgba(255,255,255,0.15)",
+    width: width > 750 ? 24 : width > 730 ? 22 : 20,
+    height: width > 750 ? 24 : width > 730 ? 22 : 20,
+    borderRadius: 20,
+    marginRight: width > 750 ? 12 : 10,
+  },
+  activeRadioButton: {
+    backgroundColor: "#28AF6E",
+    width: width > 750 ? 24 : width > 730 ? 22 : 20,
+    height: width > 750 ? 24 : width > 730 ? 22 : 20,
+    borderRadius: 20,
+    marginRight: width > 750 ? 12 : 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  activeRadioDot: {
+    backgroundColor: "#FFFFFF",
+    padding: 3.5,
+    borderRadius: 20,
+  },
+  closeScreen: {
+    position: "absolute",
+    top: 30,
+    right: 0,
+    padding: 16,
+    zIndex: 1,
   },
 });
