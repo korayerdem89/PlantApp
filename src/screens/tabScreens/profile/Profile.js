5871825307b71ab8288cbcab8d5e22e26c143ef4
koray kilic <korayerdem89@gmail.com>
2023-11-20 01:05:50 +0300

    tab screens changed

--- src/screens/tabScreens/profile/Profile.js
+++ src/screens/tabScreens/profile/Profile.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Dimensions, View } from "react-native";
 import React from "react";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+
+const { width, height } = Dimensions.get("window");
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView style={styles.rootContainer}>
+      <View style={styles.container}>
+        <Text>Profile</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: { flex: 1, paddingTop: getStatusBarHeight() + 12 },
+  container: {
+    flex: 1,
+    paddingHorizontal: width > 720 ? 24 : 20,
+  },
+});
--- src/screens/tabScreens/scan/Scan.js
+++ src/screens/tabScreens/scan/Scan.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Dimensions, View } from "react-native";
 import React from "react";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+
+const { width, height } = Dimensions.get("window");
 
 const Scan = () => {
   return (
-    <View>
-      <Text>Scan</Text>
-    </View>
+    <SafeAreaView style={styles.rootContainer}>
+      <View style={styles.container}>
+        <Text>Scan</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Scan;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: { flex: 1, paddingTop: getStatusBarHeight() + 12 },
+  container: {
+    flex: 1,
+    paddingHorizontal: width > 720 ? 24 : 20,
+  },
+});
