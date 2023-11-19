711d997c18606a42745d38cd688ce2a700bf05b6
koray kilic <korayerdem89@gmail.com>
2023-11-20 00:48:59 +0300

    bottomTab is ok

--- src/bottomTabNavigator/BottomTabNavigator.js
+++ src/bottomTabNavigator/BottomTabNavigator.js
@@ -1,30 +1,124 @@
+import React, { useRef } from "react";
+import { Animated, Image, TouchableWithoutFeedback, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useNavigation } from "@react-navigation/native";
 import Home from "../screens/tabScreens/home";
 import Diagnose from "../screens/tabScreens/diagnose";
 import Scan from "../screens/tabScreens/scan";
 import MyGarden from "../screens/tabScreens/myGarden";
 import Profile from "../screens/tabScreens/profile";
 import HomeIcon from "../../assets/BottomTabIcons/HomeIcon";
+import DiagnoseIcon from "../../assets/BottomTabIcons/DiagnoseIcon";
+import MyGardenIcon from "../../assets/BottomTabIcons/MyGardenIcon";
+import ProfileIcon from "../../assets/BottomTabIcons/ProfileIcon";
+import { colors } from "../constants/theme";
 const Tab = createBottomTabNavigator();
 
 function BottomTabNavigator() {
+  const navigation = useNavigation();
+
+  //Scan Button için scale animasyonu
+  const tabBarScale = useRef(new Animated.Value(1)).current;
+
+  const handlePressIn = () => {
+    Animated.timing(tabBarScale, {
+      toValue: 0.9,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handlePressOut = () => {
+    Animated.timing(tabBarScale, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+  //////////////////////////////////////
+
   return (
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarInactiveTintColor: "#BDBDBD",
+        tabBarActiveTintColor: colors.primary,
+        tabBarStyle: { height: 60, paddingBottom: 10 },
+        tabBarLabelStyle: {
+          fontFamily: "Rubik-Regular",
+        },
       }}
     >
       <Tab.Screen
         options={{
-          tabBarIcon: ({ color }) => <HomeIcon color={"red"} />,
+          tabBarIcon: ({ focused }) => (
+            <HomeIcon
+              width={25}
+              height={25}
+              color={focused ? colors.primary : "#BDBDBD"}
+            />
+          ),
+          tabBarLabelStyle: { right: 1 },
         }}
         name="Home"
         component={Home}
       />
-      <Tab.Screen name="Diagnose" component={Diagnose} />
-      <Tab.Screen name="Scan" component={Scan} />
-      <Tab.Screen name="MyGarden" component={MyGarden} />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <DiagnoseIcon color={focused ? colors.primary : "#BDBDBD"} />
+          ),
+        }}
+        name="Diagnose"
+        component={Diagnose}
+      />
+      <Tab.Screen
+        name="Scan"
+        component={Scan}
+        options={{
+          tabBarIcon: () => (
+            <Animated.View style={{ transform: [{ scale: tabBarScale }] }}>
+              <TouchableWithoutFeedback
+                onPressIn={handlePressIn}
+                onPressOut={handlePressOut}
+                onPress={() => navigation.navigate("Scan")}
+              >
+                <View>
+                  <Image
+                    source={require("../../assets/BottomTabIcons/ScanButton.png")}
+                    style={{
+                      width: 74,
+                      height: 64,
+                      bottom: 17,
+                      zIndex: 2,
+                    }}
+                  />
+                </View>
+              </TouchableWithoutFeedback>
+            </Animated.View>
+          ),
+          tabBarLabelStyle: { display: "none" },
+        }}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <MyGardenIcon color={focused ? colors.primary : "#BDBDBD"} />
+          ),
+          tabBarLabel: "My Garden",
+        }}
+        name="MyGarden"
+        component={MyGarden}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <ProfileIcon color={focused ? colors.primary : "#BDBDBD"} />
+          ),
+        }}
+        name="Profile"
+        component={Profile}
+      />
     </Tab.Navigator>
   );
 }
--- src/screens/tabScreens/home/Home.js
+++ src/screens/tabScreens/home/Home.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, SafeAreaView, Dimensions, View } from "react-native";
 import React from "react";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+
+const { width, height } = Dimensions.get("window");
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.rootContainer}>
+      <View style={styles.container}>
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: { flex: 1, paddingTop: getStatusBarHeight() + 12 },
+  container: {
+    flex: 1,
+    paddingHorizontal: width > 720 ? 24 : 20,
+  },
+});
